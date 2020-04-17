0c5f4b176d147be3e1a00c64f863be0a130d2424
Caspar Meijer <capsar.meijer@gmail.com>
2020-04-17 14:09:17 +0200

    Little improvement in speed by reshaping before training.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import z_helper as h
+import time
+
 
 class NeuralNetwork:
 
@@ -8,18 +10,17 @@ class NeuralNetwork:
         assert len(layer_sizes)-1 == len(layer_activations)
 
         # Initialize weights between every neuron in all adjacent layers.
-        self.weights = [h.random_np(low, high, (layer_sizes[i-1], layer_sizes[i])) for i in range(1, len(layer_sizes))]
+        self.weights = [np.random.uniform(low, high, (layer_sizes[i-1], layer_sizes[i])) for i in range(1, len(layer_sizes))]
         # Initialize biases for every neuron in all layers
-        self.biases = np.array([h.random_np(low, high, layer_sizes[i]).reshape(-1, 1) for i in range(1, len(layer_sizes))])
+        self.biases = np.array([np.random.uniform(low, high, (layer_sizes[i], 1)) for i in range(1, len(layer_sizes))])
         # Initialize empty list of output of every neuron in all layers.
-        self.layer_outputs = [np.zeros(layer_sizes[i]).reshape(-1, 1) for i in range(len(layer_sizes))]
+        self.layer_outputs = [np.zeros((layer_sizes[i], 1)) for i in range(len(layer_sizes))]
 
         self.layer_activations = layer_activations
         self.layer_sizes = layer_sizes
         self.learning_rate = learning_rate
 
     def calculate_output(self, input_data):
-        input_data = np.array(input_data).reshape(-1, 1)
         assert len(input_data) == self.layer_sizes[0]
         num_calculations = len(self.weights)
 
@@ -27,30 +28,26 @@ class NeuralNetwork:
         self.layer_outputs[0] = y
 
         for i in range(num_calculations):
-            y = h.activation(self.layer_activations[i])(np.dot(self.weights[i].T, y) + self.biases[i])
+            y = self.layer_activations[i](np.dot(self.weights[i].T, y) + self.biases[i], False)
             self.layer_outputs[i+1] = y
 
         return y
 
     def train(self, input_data, desired_output_data):
-        input_data = np.array(input_data).reshape(-1, 1)
-        desired_output_data = np.array(desired_output_data).reshape(-1, 1)
         assert len(input_data) == self.layer_sizes[0]
         assert len(desired_output_data) == self.layer_sizes[-1]
         self.calculate_output(input_data)
 
-        error = (desired_output_data - self.layer_outputs[-1]) * h.derivative(self.layer_activations[-1])(self.layer_outputs[-1])
+        error = (desired_output_data - self.layer_outputs[-1]) * self.layer_activations[-1](self.layer_outputs[-1], True)
         self.weights[-1] += (self.learning_rate * self.layer_outputs[-2] * error.T)
         self.biases[-1] += self.learning_rate * error
 
         for i in reversed(range(len(self.weights)-1)):
-            error = np.dot(self.weights[i+1], error) * h.derivative(self.layer_activations[i])(self.layer_outputs[i+1])
+            error = np.dot(self.weights[i+1], error) * self.layer_activations[i](self.layer_outputs[i+1], True)
             self.weights[i] += (self.learning_rate * self.layer_outputs[i] * error.T)
             self.biases[i] += self.learning_rate * error
-    
+
     def calculate_SSE(self, input_data, desired_output_data):
-        input_data = np.array(input_data).reshape(-1, 1)
-        desired_output_data = np.array(desired_output_data).reshape(-1, 1)
         assert len(input_data) == self.layer_sizes[0]
         assert len(desired_output_data) == self.layer_sizes[-1]
         return np.sum(np.power(desired_output_data - self.calculate_output(input_data), 2))
@@ -61,32 +58,35 @@ class NeuralNetwork:
 
 
 np.set_printoptions(linewidth=200)
-for i in range(5):
+
+data_input = h.import_from_csv("data/features.txt", float)
+data_output = h.import_from_csv("data/targets.txt", int)
+data_output = np.array([h.class_to_array(np.amax(data_output), x) for x in data_output])
+
+data_input = data_input.reshape((len(data_input), -1, 1))
+data_output = data_output.reshape((len(data_input), -1, 1))
+
+for i in range(4):
     random_seed = np.random.randint(10, 1010)
     np.random.seed(random_seed)
-
-    data_input = h.import_from_csv("data/features.txt", float)
-    data_output = h.import_from_csv("data/targets.txt", int)
-    data_output = np.array([h.class_to_array(np.amax(data_output), x) for x in data_output])
 
     train_input, validate_input, test_input = h.kfold(4, data_input, random_seed)
     train_output, validate_output, test_output = h.kfold(4, data_output, random_seed)
 
-    nn = NeuralNetwork(layer_sizes=[10, 15, 7], layer_activations=["sigmoid", "sigmoid"])
+    nn = NeuralNetwork(layer_sizes=[10, 15, 7], layer_activations=[h.sigmoid, h.sigmoid])
 
     previous_mse = 1
     current_mse = 0
     epochs = 0
+    begin_time = time.time_ns()
     while(current_mse < previous_mse):
+        epochs += 1
         previous_mse = h.calculate_MSE(nn, validate_input, validate_output)
         for i in range(len(train_input)):
             nn.train(train_input[i], train_output[i])
         current_mse = h.calculate_MSE(nn, validate_input, validate_output)
-        
-        epochs += 1
-        # if epochs % 10 == 0: print("Epoch: " + str(epochs) + " MSE: " + str(current_mse))
-
+    end_time = time.time_ns()
 
     train_mse = h.calculate_MSE(nn, train_input, train_output)
     test_mse = h.calculate_MSE(nn, test_input, test_output)
-    print("Random_Seed: "  + str(random_seed) + " Epochs: " + str(epochs) + " Tr: " + str(train_mse) + " V: " + str(current_mse) + " T: " + str(test_mse))
+    print("Seed:", random_seed, "Epochs:", epochs, "Time:", (end_time-begin_time)/1e9, "Tr:", train_mse, "V:", current_mse, "T:", test_mse)
--- z_helper.py
+++ z_helper.py
@@ -1,18 +1,22 @@
 import numpy as np
 
+
 def import_from_csv(path, data_type):
-    return  np.genfromtxt(path, dtype=data_type, delimiter=',') 
+    return np.genfromtxt(path, dtype=data_type, delimiter=',')
+
 
 def class_to_array(maximum_class, x):
     data = np.zeros(maximum_class)
     data[x-1] = 1
-    return data 
+    return data
+
 
 def kfold(k, data, seed=99):
     np.random.seed(seed)
-    np.random.shuffle(data)
+    data = np.random.permutation(data)
     fold_size = int(len(data) / k)
     return data[:fold_size], data[fold_size:fold_size*2], data[fold_size*2:]
+
 
 def calculate_MSE(nn, input_data, output_data):
     size = len(input_data)
@@ -21,40 +25,27 @@ def calculate_MSE(nn, input_data, output_data):
         sum_error += nn.calculate_SSE(input_data[i], output_data[i])
     return sum_error / size
 
+
 def random_np(low, high, size):
     assert low <= high
     return np.random.random(size)*(high-low) + low
 
-def activation(s):
-    if s == "relu":
-        return relu
-    if s == "sigmoid":
-        return sigmoid
+
+def sigmoid(x, derivative):
+    if derivative:
+        return x * (1.0 - x)
     else:
-        return "Error"
+        return 1.0 / (1.0 + np.exp(-x))
 
-def derivative(s):
-    if s == "relu":
-        return relu_derivative
-    if s == "sigmoid":
-        return sigmoid_derivative
+
+def relu(x, derivative):
+    if derivative:
+        x[x <= 0] = 0
+        x[x > 0] = 1
+        return x
     else:
-        return "Error"
+        return np.maximum(0, x)
 
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
-
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
-
-def relu(x):
-    return np.maximum(0, x)
-
-def relu_derivative(x):
-    x[x<=0] = 0
-    x[x>0] = 1
-    return x
 
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x))
